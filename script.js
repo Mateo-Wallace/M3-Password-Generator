1a2a16663288d5207f2eac762227cdeedf551328
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-10-02 18:21:48 -0400

    redid password character if else statements

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ function generatePassword() {
   alert ('Follow the following commands in order to generate your random password.');
 
   // User input for password length and validation for a number between specified values
-  
   var inValid = true;
   while (inValid) {
     var passwordLength = Number(prompt('How many characters would you like your password to be?\nMust be more than 8 characters.\nMust be less than 128 characters.'));
@@ -37,23 +36,39 @@ function generatePassword() {
     var lowerCase = confirm('Would you like your password to include: \nLower Case letters?');
     var upperCase = confirm('Would you like your password to include: \nUpper Case letters?');
     var numeric = confirm('Would you like your password to include: \nNumbers?');
-    var specialChar = confirm('Would you like your password to include: \nSpecial Characters? \nEx: ?, !, >');
-    alert('You chose: \n Lower Case Letters: ' + lowerCase + '\n Upper Case Letters: ' + upperCase + '\n Numbers: ' + numeric + '\n Special Characters: ' + specialChar)
-    if (lowerCase == true || upperCase == true || numeric == true || specialChar == true) {
+    var special = confirm('Would you like your password to include: \nSpecial Characters? \nEx: ?, !, >');
+    alert('You chose: \n Lower Case Letters: ' + lowerCase + '\n Upper Case Letters: ' + upperCase + '\n Numbers: ' + numeric + '\n Special Characters: ' + special);
+    if (lowerCase == true || upperCase == true || numeric == true || special == true) {
       inValid = true;
     } else {
-      alert('At least one character type must be selected. Please try again')
+      alert('At least one character type must be selected. Please try again');
     }
   }
   
-
   // Defines available password characters based on user input
+  if (lowerCase == true) {
+    lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+  } else {
+    lowerCase = ''
+  } if (upperCase == true) {
+    upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  } else {
+    upperCase = ''
+  } if (numeric == true) {
+    numeric = '123456789'
+  } else {
+    numeric = ''
+  } if (special == true) {
+    special = ' ~`!@#$%^&*()-+={}[]|/:;<>,.?'
+  } else {
+    special = ''
+  }
+
+  // Merges strings of all chosen options
+  var grandString = special.concat(lowerCase, upperCase, numeric);
 
 
-
-
-
-  return specialChar;
+  return grandString;
 }
 
 // add rng 
